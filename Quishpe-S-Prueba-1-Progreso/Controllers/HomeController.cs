6d995aa6a97edce5aaafe702b085c05068e3615d
Stheeven Quishpe <stheeven.quishpe@udla.edu.ec>
2025-05-01 07:14:46 -0500

    Agregar archivos de proyecto.

---
+++ Quishpe-S-Prueba-1-Progreso/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Quishpe_S_Prueba_1_Progreso.Models;
+
+namespace Quishpe_S_Prueba_1_Progreso.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Quishpe-S-Prueba-1-Progreso/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Quishpe_S_Prueba_1_Progreso.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
